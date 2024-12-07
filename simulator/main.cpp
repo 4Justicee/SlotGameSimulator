4b9caaef8746c5b393d2d2da9abd98873c8940ba
4Justicee <kcstar914@outlook.com>
2024-12-07 13:43:04 -0500

    single

--- simulator/main.cpp
+++ simulator/main.cpp
@@ -10,25 +10,6 @@
 
 using namespace std;
 
-vector<vector<int>> fs_reels = {
-	  {1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 11, 11, 11},      // fs_reel1  
-	  {1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 8, 8, 8, 9, 9, 10, 10, 10, 10, 10, 11}, // fs_reel2  
-	  {2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 9, 10}, // fs_reel3  
-	  {2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 9, 9, 10, 11, 11}, // fs_reel4  
-	  {1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 10, 10, 10, 10, 10, 11}, // fs_reel5  
-	  {2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8, 9, 10, 10, 11, 11, 11, 11}  // fs_reel6  
-};
-
-vector<vector<int>> main_reels = {
-	{ 1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,9,9,10,10 },
-	{ 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10 },
-	{ 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10 },
-	{ 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 10, 10 },
-	{ 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 9, 10, 10 },
-	{ 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 9, 9, 9, 10 }
-};
-
-
 map<int, map<int, double>> payTable{
 	{1, {{4, 5}, {5, 10}, {6, 25}, {7, 25}, {8, 25}, {9, 25}, {10, 25}, {11, 25}, {12, 25}}},
 	{2, {{8, 0.4}, {9, 0.4}, {10, 1.5}, {11, 1.5}, {12, 5}}},
@@ -57,27 +38,23 @@ int bomb[] = {
 	50
 };
 
+struct ResultData {
+	double total_win;
+	std::vector<std::vector<int>> win_symbols;
 
-int freeSpinRemain = 0;
-int gameMode = 0;
-double totalWin = 0;
-
-double normalwin = 0;
-double freewin = 0;
-double freebase = 0;
-int freewincount = 0;
-
-vector<vector<int>> symbolRates = {
-	{0,0,0,0,0,0,0,0,0} ,
-	{0,0,0,0,0},
-	{0,0,0,0,0},
-	{0,0,0,0,0},
-	{0,0,0,0,0},
-	{0,0,0,0,0},
-	{0,0,0,0,0},
-	{0,0,0,0,0},
-	{0,0,0,0,0},
-	{0,0,0,0,0},
+	// Default constructor  
+	ResultData() : total_win(0), win_symbols({
+		{0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0}
+		}) {}
 };
 
 struct SymbolData {
@@ -113,7 +90,7 @@ void shuffleReels(vector<vector<int>>& reels) {
 	// shuffle(reels.begin(), reels.end(), engine);  
 }
 
-vector<pair<int, int>> checkViewAndGetWins(const vector<vector<int>>& view) {
+vector<pair<int, int>> checkViewAndGetWins(const vector<vector<int>>& view, int &gameMode, ResultData* result) {
 	const int defaultThreshold = 8;
 	unordered_map<int, int> specialThreshold = { {1, 4} };
 
@@ -151,9 +128,8 @@ vector<pair<int, int>> checkViewAndGetWins(const vector<vector<int>>& view) {
 
 			if (payTable[symbol][data.count] != 0) {
 				double win = payTable[symbol][data.count];
-				totalWin += win;
-				normalwin += win;
-				symbolRates[symbol - 1][data.count - threshold]++;
+				result->total_win += win;
+				result->win_symbols[symbol - 1][data.count - threshold]++;
 
 				if (symbol == 1) {
 					gameMode = 1;
@@ -205,20 +181,20 @@ void dropSymbols(vector<vector<int>>& view, vector<int>& bottomNumber, const vec
 	}
 }
 
-void tumble(vector<vector<int>>& view, vector<int>& bottomNumber) {
+void tumble(vector<vector<int>>& view, vector<int>& bottomNumber, int &gameMode, vector<vector<int>> reelset, ResultData* result) {
 	bool anyWins;
 
 	do {
-		vector<pair<int, int>> wins = checkViewAndGetWins(view);
+		vector<pair<int, int>> wins = checkViewAndGetWins(view, gameMode, result);
 		anyWins = !wins.empty();
 		if (anyWins) {
 			removeWinsFromView(view, wins);
-			dropSymbols(view, bottomNumber, main_reels);
+			dropSymbols(view, bottomNumber, reelset);
 		}
 	} while (anyWins && gameMode == 0);
 }
 
-void baseModePlay() {
+void baseModePlay(int & gameMode, vector<vector<int>> reelset, ResultData *result) {
 	const int rows = 5;
 	const int reels = 6;
 	vector<vector<int>> view(rows, vector<int>(reels));
@@ -227,7 +203,7 @@ void baseModePlay() {
 	// Precompute the sizes of each reel  
 	vector<int> reelSizes(reels);
 	for (int j = 0; j < reels; ++j) {
-		reelSizes[j] = main_reels[j].size();
+		reelSizes[j] = reelset[j].size();
 		bottomNumber[j] = randomInt(0, reelSizes[j] - 1); // Select a random start index for each reel  
 	}
 
@@ -235,7 +211,7 @@ void baseModePlay() {
 	for (int i = 0; i < rows; ++i) {
 		for (int j = 0; j < reels; ++j) {
 			int index = (bottomNumber[j] + i) % reelSizes[j];
-			view[rows - 1 - i][j] = main_reels[j][index];
+			view[rows - 1 - i][j] = reelset[j][index];
 		}
 	}
 
@@ -244,10 +220,10 @@ void baseModePlay() {
 		bottomNumber[j] = (bottomNumber[j] + rows) % reelSizes[j];
 	}
 
-	tumble(view, bottomNumber);  // Ensure this function is optimized as well  
+	tumble(view, bottomNumber, gameMode, reelset, result);  // Ensure this function is optimized as well  
 }
 
-WinResult freeCheckWin(const vector<vector<int>>& view) {
+WinResult freeCheckWin(const vector<vector<int>>& view, int & free_spin_remain, ResultData * p) {
 	int rows = 5;
 	int reels = 6;
 	const int defaultThreshold = 8;
@@ -282,7 +258,7 @@ WinResult freeCheckWin(const vector<vector<int>>& view) {
 		int symbol = item.first;
 		SymbolData& data = item.second;
 		if (symbol == 1 && data.count >= 3) {
-			freeSpinRemain += 5;
+			free_spin_remain += 5;
 		}
 		int threshold = specialThreshold.count(symbol) ? specialThreshold.at(symbol) : defaultThreshold;
 
@@ -291,8 +267,7 @@ WinResult freeCheckWin(const vector<vector<int>>& view) {
 			if (payTable[symbol][data.count] != 0) {
 				double win = static_cast<double>(payTable[symbol][data.count]);
 				result.winValue += win;				
-				freebase += win;
-				symbolRates[symbol-1][data.count - threshold]++;
+				p->win_symbols[symbol-1][data.count - threshold]++;
 			}
 		}
 	}
@@ -310,9 +285,10 @@ double determineMultiFromView(const vector<vector<int>>& view) {
 	for (int i = 0; i < rows; ++i) {
 		for (int j = 0; j < reels; ++j) {
 			if (view[i][j] == 11) {
+		
+				int randomIndex = randomInt(0, 257);
 				
-				int randomIndex = randomInt(0, 257);				
-				multiplier += bomb[randomIndex];			
+				multiplier += bomb[randomIndex];				
 
 			}
 		}
@@ -321,97 +297,126 @@ double determineMultiFromView(const vector<vector<int>>& view) {
 	return (multiplier == 0) ? 1 : multiplier; // Return 1 if no multiSymbols are found; otherwise, return the accumulated multiplier  
 }
 
-void freeTumble(vector<vector<int>>& view, vector<int>& bottomNumber) {
+void freeTumble(vector<vector<int>>& view, vector<int>& bottomNumber, int &free_spin_remain, vector<vector<int>>  reelset, ResultData *result) {
 	bool anyWins = false;
 	double winValue = 0;
 
 	do {
-		WinResult winResult = freeCheckWin(view); // Evaluate the current view for wins  
+		WinResult winResult = freeCheckWin(view, free_spin_remain, result); // Evaluate the current view for wins  
 		const auto& wins = winResult.wins;
 		const double win = winResult.winValue;
 
 		anyWins = !wins.empty(); // Check if there are any wins  
 		if (anyWins) {
 			removeWinsFromView(view, wins); // Remove win positions by setting them to a predefined null value e.g., -1  
-			dropSymbols(view, bottomNumber, fs_reels); // Let symbols above drop down  
+			dropSymbols(view, bottomNumber, reelset); // Let symbols above drop down  
 			winValue += win; // Accumulate win value  
 		}
 	} while (anyWins);  // If there were wins, recheck the new view state  
 
 	// Apply multiplier and update total win if there were any wins  
 	if (winValue != 0) {
-		freewincount++;
 		double multiplier = determineMultiFromView(view); // Calculate a multiplier based on the current view
-	
-		totalWin += multiplier * winValue; // Add to the total win counter 
-		freewin += multiplier * winValue;
+		result->total_win += multiplier * winValue; // Add to the total win counter 		
 	}
 }
 
 
-void freeModePlay() {
+void freeModePlay(vector<vector<int>>  reelset, ResultData * result) {
 	int totalPlayed = 0;
 	int rows = 5;
 	int reels = 6;
-	freeSpinRemain = 10;
+	int free_spin_remain = 10;
 
-	while (freeSpinRemain > 0 && totalPlayed < 500) {
+	while (free_spin_remain > 0 && totalPlayed < 500) {
 
 		vector<vector<int>> view(rows, vector<int>(reels));
 		vector<int> bottomNumber(reels);
 		for (int j = 0; j < reels; j++) {
-			bottomNumber[j] = randomInt(0, fs_reels[j].size());
+			bottomNumber[j] = randomInt(0, reelset[j].size());
 		}
 
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < reels; j++) {
-				int idx = (bottomNumber[j] + i) % fs_reels[j].size();
-				view[rows - 1 - i][j] = fs_reels[j][idx];
+				int idx = (bottomNumber[j] + i) % reelset[j].size();
+				view[rows - 1 - i][j] = reelset[j][idx];
 			}
 		}
 
 		for (int i = 0; i < reels; i++) {
-			bottomNumber[i] = (bottomNumber[i] + rows) % fs_reels[i].size();
+			bottomNumber[i] = (bottomNumber[i] + rows) % reelset[i].size();
 		}
 		totalPlayed++;
-		freeTumble(view, bottomNumber);
-		freeSpinRemain--;
+		freeTumble(view, bottomNumber, free_spin_remain, reelset, result);
+		free_spin_remain--;
 	}
 }
 
-void simulateGame(const int totalPlayCount) {
-	#pragma omp parallel  
-	{
-		vector<vector<int>> local_main_reels = main_reels;
-		vector<vector<int>> local_fs_reels = fs_reels;
-		#pragma omp for reduction(+:totalWin,normalwin,freewin,freebase,freewincount) private(gameMode)
+void mergeResults(ResultData* finalResult, ResultData* threadResult) {
+	// Sum up the total wins  
+	finalResult->total_win += threadResult->total_win;
 
-		for (int playCount = 0; playCount < totalPlayCount; playCount++) {
-			if (gameMode == 0) {
-				shuffleReels(local_main_reels);
-				baseModePlay();
-		
-			}
-			else {
-				shuffleReels(local_fs_reels);
-				freeModePlay();
-				gameMode = 0;
-	
-			}
+	// Sum up the win symbols by iterating over the matrix  
+	for (size_t i = 0; i < threadResult->win_symbols.size(); ++i) {
+		for (size_t j = 0; j < threadResult->win_symbols[i].size(); ++j) {
+			finalResult->win_symbols[i][j] += threadResult->win_symbols[i][j];
 		}
 	}
 }
+
+void simulateGame(int total) {
+
+
+		int gameMode = 0;
+		ResultData result;
+
+		vector<vector<int>> fs_reels = {
+			  {1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 11, 11, 11},      // fs_reel1  
+			  {1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 8, 8, 8, 9, 9, 10, 10, 10, 10, 10, 11}, // fs_reel2  
+			  {2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 9, 10}, // fs_reel3  
+			  {2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 9, 9, 10, 11, 11}, // fs_reel4  
+			  {1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 10, 10, 10, 10, 10, 11}, // fs_reel5  
+			  {2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 8, 9, 10, 10, 11, 11, 11, 11}  // fs_reel6  
+		};
+
+		vector<vector<int>> main_reels = {
+			{ 1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,9,9,10,10 },
+			{ 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10 },
+			{ 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10 },
+			{ 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 8, 9, 9, 10, 10 },
+			{ 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 9, 9, 10, 10 },
+			{ 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 8, 8, 9, 9, 9, 10 }
+		};
+
+		
+		
+
+		for (int playCount = 0; playCount < total; playCount++) {
+
+			if (gameMode == 0) {
+				shuffleReels(main_reels);
+				baseModePlay(gameMode, main_reels, &result);
+			}
+			else {
+				shuffleReels(fs_reels);
+				freeModePlay(fs_reels, &result);
+				gameMode = 0;
+			}
+		}
+
+
+	cout << result.total_win << '\n';
+}
+
 int main(int argc, char ** argv) 
 {
-	int totalPlayCount = 100000;
-	int playCount = 0;
-	omp_set_num_threads(omp_get_num_procs());
+	int totalPlayCount = 1000000;	
 
-	
-	//#pragma omp parallel for
 	double totaltime = 0;
 	auto start = chrono::high_resolution_clock::now();
-	simulateGame(100000);
+	
+	simulateGame(totalPlayCount);
+
 	auto finish = chrono::high_resolution_clock::now();
 	chrono::duration<double, milli> elapsed = finish - start;
 	cout << elapsed.count();
